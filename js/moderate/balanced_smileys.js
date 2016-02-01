d335c2ba31229e9bbd0025a513b91e62bd81543c
Timothee Groleau <tgroleau@zendesk.com>
2016-02-01 21:53:44 +0800

    Optimizations for balanced smiley (reduce recursion calls to minimum)

--- js/moderate/balanced_smileys.js
+++ js/moderate/balanced_smileys.js
@@ -1,79 +1,40 @@
-var fs  = require("fs");
-fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line)
-{
-	if (line === "") return;
+require("fs")
+	.readFileSync(process.argv[2])
+	.toString()
+	.split('\n')
+	.forEach(function (line)
+	{
+		if (!line) return;
 
-	process.stdout.write((isBraceBalanced(line) ? 'YES' : 'NO') + '\n');
-});
-
+		process.stdout.write((isBraceBalanced(line) ? 'YES' : 'NO') + '\n');
+	});
 
 // recursive strategy
-function isBraceBalanced(str, from_idx, stack) {
+function isBraceBalanced(str, from_idx, opened) {
 	if (!from_idx) from_idx = 0;
-	if (!stack)    stack    = [];
+	if (opened === undefined) opened = 0;
 
 	for (var idx=from_idx, len=str.length; idx<len; idx++)
 	{
-		var c = str.charAt(idx);
-		if (c === '(')
-		{
-			stack.push(c);
-		}
-		else if (c === ')') {
-			if (stack.length <= 0) return false;
-			stack.pop();
-		}
-		else if (c === ':') {
-			if (idx+1 < len) {
-				var c2 = str.charAt(idx + 1); // look ahead
-				if (c2 === ')') {
-					if (stack.length) {
+		switch( str.charAt(idx) ) {
+			case '(':
+				opened++;
+				break;
+			case ')':
+				if (--opened < 0) return false;
+				break;
+			case ':':
+				switch ( str.charAt(idx + 1) ) { // look ahead (js doesn't throw out-of-bounds)
+					case ')':
+					case '(':
 						// possible collision
-						return (
-							isBraceBalanced(str, idx + 2, stack.concat()) // smiley consumed
-							||
-							isBraceBalanced(str, idx + 2, stack.slice(0, -1)) // brace closes old block, pop stack
-						);
-					}
-					idx++; // brace is consumed by smiley
+						if (isBraceBalanced(str, idx + 2, opened)) // smiley consumed
+						{
+							return true;
+						}
 				}
-				else if (c2 === '(') {
-					// possible branch
-					return (
-						isBraceBalanced(str, idx + 2, stack.concat()) // smiley consumed
-						||
-						isBraceBalanced(str, idx + 2, stack.concat(['('])) // brace opens new block, push to stack
-					);
-				}
-			}
 		}
 	}
 
-	return stack.length === 0;
+	return opened === 0;
 }
-
-/* Invalid solution!! Consumes braces which *could* come from blocks instead of smileys!!
-function isBraceBalanced2(original_str) {
-
-	str = original_str.replace(/:[\(\)]/g, '');
-	console.log(original_str);
-	console.log(str);
-
-	var stack = [];
-
-	for (var idx=0, len=str.length; idx<len; idx++) {
-		var c = str.charAt(idx);
-
-		if (c === '(')
-		{
-			stack.push(c);
-		}
-		else if (c === ')') {
-			if (stack.length <= 0) return false;
-			stack.pop();
-		}
-	}
-
-	return stack.length === 0;
-}
-/**/
