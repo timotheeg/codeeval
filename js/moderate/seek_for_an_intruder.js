1fbd841cb4f22e029dedc91eaa0d7e58cfa66da3
Timothee Groleau <tg@timotheegroleau.com>
2016-02-04 22:39:14 +0800

    Seek for An Intruder - Passed!

--- js/moderate/seek_for_an_intruder.js
+++ js/moderate/seek_for_an_intruder.js
@@ -1,53 +1,32 @@
-var fs  = require("fs");
-var snippet = fs.readFileSync(process.argv[2]).toString();
-
-var min_ip_allowed = 1 << 24;
-var max_ip_allowed = (255<<24 | 255<<16 | 255<<8 | 254)>>>0;
+var snippet = require("fs").readFileSync(process.argv[2]).toString();
 
 function to_ip_int(a, b, c, d) {
-	console.log(a, b, c, d);
-	if ( arguments.length === 4 )
-	{
-		if (	
-			   a < 1 || a > 255
-			|| b < 0 || b > 255
-			|| c < 0 || c > 255
-			|| d < 0 || d > 255
-		)
-		throw new Error('invalid ip');
-
-		a = (a << 24 | b << 16 | c << 8 | d)>>>0;
-	}
-
-	if (a < min_ip_allowed || a > max_ip_allowed)
-		throw new Error('ip out of range ' + [a, min_ip_allowed, max_ip_allowed, a < min_ip_allowed, a > max_ip_allowed]);
-
-	return a;
+	return (a << 24 | b << 16 | c << 8 | d)>>>0;
 }
 
 function int_to_ip(n) {
 	return (
-		+ ((n>>>24) && 0xff)
+		+ ((n>>>24) & 0xff)
 		+ '.'
-		+ ((n>>>16) && 0xff)
+		+ ((n>>>16) & 0xff)
 		+ '.'
-		+ ((n>>>8) && 0xff)
+		+ ((n>>>8) & 0xff)
 		+ '.'
-		+ (n && 0xff)
+		+ (n & 0xff)
 	);
 }
 
 var searches = [
-	{ // dotted_binary
-		re: "[01]{32}", 
-		f: function(n) { return to_ip_int(parseInt(n, 2)); }
-	},
 	{ // binary
+		re: "[01]{32}", 
+		f: function(n) { return parseInt(n, 2); }
+	},
+	{ // dotted binary
 		re: "[01]{8}(\\.[01]{8}){3}",
-		f: function(n) { return to_ip_int(parseInt(n.replace(/\./g, ''), 2)); }
+		f: function(n) { return parseInt(n.replace(/\./g, ''), 2); }
 	},
 	{ // dotted_hex
-		re: "0x[0-9A-F]{1,2}(\\.0x[0-9A-F]{1,2}){3}",
+		re: "0x[0-9A-F]{2}(\\.0x[0-9A-F]{2}){3}",
 		f: function(n) {
 			var t = n.substr(2).split('.0x');
 			return to_ip_int(
@@ -59,45 +38,45 @@ var searches = [
 		}
 	},
 	{ // hex
-		re: "0x[0-9A-F]{7,}",
+		re: "0x[0-9A-F]{8}",
 		f: function(n) { return to_ip_int(parseInt(n.substr(2), 16)); }
 	},
 	{ // dotted_octal
-		re: "0[0-7]{1,3}(\\.0[0-7]{1,3}){3}",
+		re: "0[0123][0-7]{2}(\\.0[0123][0-7]{2}){3}",
 		f: function(n) {
 			var t = n.substr(1).split('.0');
 			return to_ip_int(
-				parseInt(t[0], 16),
-				parseInt(t[1], 16),
-				parseInt(t[2], 16),
-				parseInt(t[3], 16)
+				parseInt(t[0], 8),
+				parseInt(t[1], 8),
+				parseInt(t[2], 8),
+				parseInt(t[3], 8)
 			);
 		}
 	},
 	{ // octal
-		re: "0[0-7]{9,}",
+		re: "0[123]?[0-7]{9,10}",
 		f: function(n) { return parseInt(n.substr(1), 8); }
 	},
 	{ // dotted_decimal
-		re: "[0-9]{1,3}(\\.[0-9]{1,3}){3}",
+		re: "(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}",
 		f: function(n) {
 			var t = n.split('.');
-			var a = parseInt(t[0], 10);
-			if (a > 255) a = parseInt(t[0].substr(1), 10); // attempt to salvage :p
 			return to_ip_int(
-				a,
+				parseInt(t[0], 10),
 				parseInt(t[1], 10),
 				parseInt(t[2], 10),
 				parseInt(t[3], 10)
 			);
 		}
-	},
+	}
+	/*,
 	{ // decimal
-		re: "([0-9]{8,})",
+		re: "([0-9]{8,10})",
 		f: function(n) {
 			return parseInt(n, 10);
 		}
 	}
+	/**/
 ];
 
 var res = searches.map(function(n)
@@ -107,49 +86,39 @@ var res = searches.map(function(n)
 	return re;
 });
 
-console.log(searches);
-
 var 
 	ips = {},
 	m,
 	re = new RegExp(res.join('|'), 'gi'),
-	max_cnt = 0, max_ip = null;
+	max_cnt = 0;
 ;
 
 while(m = re.exec(snippet))
 {
-	process.stdout.write(m[0] + '\n');
-
 	for (var i=0, len=searches.length; i<len; i++)
 	{
 		if (searches[i].re.test(m[0]))
 		{
-			console.log("match! " + i)
-			try
-			{
-				var ip = searches[i].f(m[0]);
-				console.log("match: " + ip);
-				var cnt = (ip in ips ? ++ips[ip] : ips[ip]=1);
-				if (cnt > max_cnt) {
-					max_cnt = cnt;
-					max_ip = ip;
-				}
-			}
-			catch(e) { console.log(e); }
+			var ip = searches[i].f(m[0]).toString(16);
+			
+			var cnt = (ip in ips ? ++ips[ip] : ips[ip]=1);
+			
+			if (cnt > max_cnt) max_cnt = cnt;
+			
 			break;
 		}
 	}
 }
 
-console.log(ips);
-
 var res = [];
 for (var ip in ips) {
-	if (ips[ip] === max_cnt) res.push(ip);
+	if (ips[ip] === max_cnt) res.push(parseInt(ip, 16));
 }
 
-console.log(max_cnt);
-console.log(res);
+process.stdout.write(res.sort(byNumAsc).map(int_to_ip).join(' ') + '\n');
 
-process.stdout.write('result:' + res.sort().map(int_to_ip).join(' ') + '\n');
+function byNumAsc(a, b)
+{
+	return a - b;
+}
 
