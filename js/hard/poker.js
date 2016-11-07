5a1207a9eee7dd8415c4cb158293665f3a9239d4
Timothee Groleau <tgroleau@zendesk.com>
2016-11-07 16:26:06 +0800

    Clean up Poker to make logic clearer

--- js/hard/poker.js
+++ js/hard/poker.js
@@ -1,61 +1,62 @@
-var fs  = require("fs");
+var
+	rank      = ['_',2,3,4,5,6,7,8,9,'T','J','Q','K','A'], // we have to use 14 symbols because no actual card should have value 0!
+	BASE      = rank.length,
+	rank_map  = {},
+	order_idx = 0,
+	orders    = {
+		          CARD_1: order_idx++,
+		          CARD_2: order_idx++,
+		          CARD_3: order_idx++,
+		          CARD_4: order_idx++,
+		          CARD_5: order_idx++,
+		        PAIR_LOW: order_idx++,
+		       PAIR_HIGH: order_idx++,
+		 THREE_OF_A_KIND: order_idx++,
+		        STRAIGHT: order_idx++,
+		           FLUSH: order_idx++,
+		 FULL_HOUSE_PAIR: order_idx++,
+		FULL_HOUSE_THREE: order_idx++,
+		  FOUR_OF_A_KIND: order_idx++,
+		  STRAIGHT_FLUSH: order_idx++
+	};
 
-var rank = ['_', 2,3,4,5,6,7,8,9,'T','J','Q','K','A'];
-var rank_map = {};
-rank.map(function(val, idx) { rank_map[val] = parseInt(idx, 10); });
-var BASE = rank.length;
-
-var order_idx = 0;
-var orders = {
-	             CARD_1: order_idx++,
-	             CARD_2: order_idx++,
-	             CARD_3: order_idx++,
-	             CARD_4: order_idx++,
-	             CARD_5: order_idx++,
-	           PAIR_LOW: order_idx++,
-	          PAIR_HIGH: order_idx++,
-	    THREE_OF_A_KIND: order_idx++,
-	      STRAIGHT_HIGH: order_idx++,
-	              FLUSH: order_idx++,
-	    FULL_HOUSE_PAIR: order_idx++,
-	   FULL_HOUSE_THREE: order_idx++,
-	     FOUR_OF_A_KIND: order_idx++,
-	STRAIGHT_FLUSH_HIGH: order_idx++
-};
+rank.forEach(function(val, idx) { rank_map[val] = idx; });
 
 function get_card(token) { return new Card(token); }
 function card_sort(ca, cb) { return ca.value - cb.value; }
 
-var lab = fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function(line) {
-	if (!line) return;
+require('fs')
+	.readFileSync(process.argv[2])
+	.toString()
+	.split('\n')
+	.forEach(function(line) {
+		if (!line) return;
 
-	cards = line.split(/\s+/).slice(0, 10).map(get_card);
+		var
+			cards = line.split(/\s+/).map(get_card),
+			hand1 = new Hand(cards.slice(0, 5)),
+			hand2 = new Hand(cards.slice(5, 10));
 
-	// console.log(line);
-
-	var hand1 = new Hand(cards.slice(0, 5));
-	// console.log(hand1);
-
-	var hand2 = new Hand(cards.slice(5));
-	// console.log(hand2);
-
-	process.stdout.write(
-		(hand1.value === hand2.value
-			? 'none'
-			: (hand1.value > hand2.value
-				? 'left'
-				: 'right'
-			)
-		) + 
-		"\n"
-	);
-
-	// console.log("==============================================");
-});
+		process.stdout.write(
+			(hand1.value === hand2.value
+				? 'none'
+				: (hand1.value > hand2.value
+					? 'left'
+					: 'right'
+				)
+			) +
+			'\n'
+		);
+	});
 
 function Hand(cards) {
-	var self = this;
-	this.value = 0;
+	var
+		self = this,
+		last_card;
+
+	this.value = 0; // value of hand is initially 0, points will be added as wining patterns are discovered
+
+	this.cards = cards.sort(card_sort); // sort cards by value
 
 	this.num_suits = 0;
 	this.suits = {};
@@ -63,42 +64,40 @@ function Hand(cards) {
 	this.num_values = 0;
 	this.values = {};
 
-	this.cards = cards.sort(card_sort);
-
-	this.flush    = true;
-	this.straight = true;
-	var last_card = null;
+	this.straight = true; // straight until proven otherwise
 
 	this.cards.forEach(function(card, idx) {
-		self.value += card.value * Math.pow(BASE, idx);
+		self.value += card.value * Math.pow(BASE, idx); // firstly, each card has a value
 
 		if (last_card) {
 			if (card.value - last_card.value != 1) {
 				self.straight = false;
 			}
 		}
+
 		last_card = card;
 
 		if (!(card.suit in self.suits)) {
 			self.suits[card.suit] = 1;
 			self.num_suits++;
 		}
-		else self.suits[card.suit]++;
+		else {
+			self.suits[card.suit]++;
+		}
 
 		if (!(card.value in self.values)) {
 			self.values[card.value] = 1;
 			self.num_values++;
 		}
-		else self.values[card.value]++;
+		else {
+			self.values[card.value]++;
+		}
 	});
 
 	this.flush = (this.num_suits === 1);
 
-	// return;
-
-	// compute value of hand
-	// value is derived as a base-13 magnitude system to assign a unique score on a hand
-	// if the score is the same, we'll resort to inspecting the individual cars in a separate algorithm
+	// Step 2, compute value of hand
+	// value is derived as a base-14 13-order magnitude system to assign a unique score to a hand
 
 	if (this.num_values === 2) {
 		// either 4 of a kind, or full house!
@@ -151,15 +150,15 @@ function Hand(cards) {
 		}
 	}
 	else {
-		// 5 different cards, either straight, flush, or nothing!
+		// 5 different cards, either straight, flush, straight-flush, or nothing!
 		if (this.straight) {
 			if (this.flush) {
-				// console.log("STRAIGHT_FLUSH_HIGH");
-				this.value += this.cards[4].value * Math.pow(BASE, orders.STRAIGHT_FLUSH_HIGH);
+				// console.log("STRAIGHT_FLUSH");
+				this.value += this.cards[4].value * Math.pow(BASE, orders.STRAIGHT_FLUSH);
 			}
 			else {
-				// console.log("STRAIGHT_HIGH");
-				this.value += this.cards[4].value * Math.pow(BASE, orders.STRAIGHT_HIGH);
+				// console.log("STRAIGHT");
+				this.value += this.cards[4].value * Math.pow(BASE, orders.STRAIGHT);
 			}
 		}
 		else if (this.flush) {
@@ -170,7 +169,7 @@ function Hand(cards) {
 }
 
 function Card(card_str) {
-	this.suit = card_str.charAt(1);
+	this.suit   = card_str.charAt(1);
 	this.figure = card_str.charAt(0);
-	this.value = rank_map[this.figure];
-};
+	this.value  = rank_map[this.figure];
+}
